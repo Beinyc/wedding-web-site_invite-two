f11fb3a892554bba49a93cad85b13906c384f635
Никита Апатенко <119950786+Beinyc@users.noreply.github.com>
2024-07-07 22:08:00 +0300

    v 0.2.2
    -переделан блок place

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/header';
 import Main from './components/main/main';
+import Place from './components/place/place';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Place/>
     </div>
   );
 }
---
+++ src/components/place/place.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./place.css";
+
+import imageTable from "../../images/table.jpg";
+import imageBouquet from "../../images/bouquet.jpg";
+
+export default function Place() {
+  return (
+    <div className="place">
+      <div className="place__permission">
+        <div className="place__cards-positions">
+          <div className="place__image-bouquet" />
+          <div className="place__cart-registration">
+            <h3 className="place__cart-title">Место регистрации</h3>
+            <p className="place__cart-paragraph_up">
+              ЗАГС
+              <br />
+              «Дворец бракосочетания»
+              <br />
+              1, микрорайон Надежда
+            </p>
+            <p className="place__cart-paragraph_up">
+              Торжественная церемония в 16:00
+            </p>
+            <a
+              className="place__map-registration"
+              href="https://yandex.ru/maps/-/CDGN6K6T"
+            >
+              Открыть карту
+            </a>
+          </div>
+        </div>
+        <div className="place__cards-positions">
+          <div className="place__image-table" />
+          <div className="place__cart-registration-down">
+            <h3 className="place__cart-title">Место проведения</h3>
+            <p className="place__cart-paragraph_up">
+              банкетный зал
+              <br />
+              «The Garag»
+              <br />
+              ул. Ерошенко, 5
+            </p>
+            <p className="place__cart-paragraph_up">Сбор гостей в 18:00</p>
+            <a
+              className="place__map-registration"
+              href="https://yandex.ru/maps/-/CDGfRC2d"
+            >
+              Открыть карту
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
